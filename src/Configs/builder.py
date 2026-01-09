c1cc063c2a686fcfef495c0b930e5dd16297171c
Murasamekiss <75076238+cyanlll@users.noreply.github.com>
2026-01-09 15:52:21 +0800

    Update builder.py

--- src/Configs/builder.py
+++ src/Configs/builder.py
@@ -1,5 +1,8 @@
 def get_configs(dataset_name):
-    if dataset_name in ['act']:
+    if dataset_name in ['tvr']:
+        import Configs.tvr as tvr
+        return tvr.get_cfg_defaults()
+    elif dataset_name in ['act']:
         import Configs.act as act
         return act.get_cfg_defaults()
     elif dataset_name in ['cha']:
