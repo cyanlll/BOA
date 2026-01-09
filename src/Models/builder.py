2ae2c25b5681e7a6ed6137f07607998e76ab72bc
Murasamekiss <75076238+cyanlll@users.noreply.github.com>
2026-01-09 15:59:01 +0800

    Update builder.py

--- src/Models/builder.py
+++ src/Models/builder.py
@@ -4,14 +4,15 @@ from Models.boa.model import BOA_Net
 
 def get_models(cfg):
     model_config = EDict(
+        text_input_size=cfg['text_feat_dim'],
         visual_input_size=cfg['visual_feat_dim'],
         query_input_size=cfg['q_feat_size'],
         hidden_size=cfg['hidden_size'],
         max_ctx_l=cfg['max_ctx_l'],
         map_size=cfg['map_size'],
         max_desc_l=cfg['max_desc_l'],
-        input_drop=0.4 if cfg['collection']=="activitynet" else 0.25,
-        drop=0.4 if cfg['collection']=="activitynet" else 0.25,
+        input_drop=cfg['input_drop'],
+        drop=cfg['drop'],
         n_heads=cfg['n_heads'],
         initializer_range=cfg['initializer_range'],
         margin=cfg['margin'],
